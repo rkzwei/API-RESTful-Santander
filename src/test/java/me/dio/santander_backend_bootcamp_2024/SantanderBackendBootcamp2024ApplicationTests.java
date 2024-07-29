427373aeed4c32fdfeec175a79016584faa102fe
rkzwei <rk@rkzwei.dev>
2024-07-28 21:10:24 -0300

    Primeiro Commit

---
+++ src/test/java/me/dio/santander_backend_bootcamp_2024/SantanderBackendBootcamp2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_backend_bootcamp_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBackendBootcamp2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
