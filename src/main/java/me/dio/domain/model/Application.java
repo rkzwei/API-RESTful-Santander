9bff17de0a4a4b5e693178138640551c4f8ee8e6
rkzwei <rk@rkzwei.dev>
2024-07-28 22:38:44 -0300

    Adicionado Interfaces e Prototipagem

--- src/main/java/me/dio/domain/model/Application.java
+++ src/main/java/me/dio/domain/model/Application.java
@@ -1,13 +1,12 @@
-package me.dio.santander_backend_bootcamp_2024;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SantanderBackendBootcamp2024Application {
+public class Application{
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderBackendBootcamp2024Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
+
